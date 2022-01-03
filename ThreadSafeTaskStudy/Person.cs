804f7f04139ae043b9afed2efd55ead3e997f15d
Vitor Luis dos Santos Silva <vitor.luisdss@gmail.com>
2022-01-03 15:21:02 -0300

    Add project files.

---
+++ ThreadSafeTaskStudy/Person.cs
@@ -0,0 +1,30 @@
+﻿namespace ThreadSafeTaskStudy
+{
+    internal class PersonClass
+    {
+        public int Code { get; set; }
+
+        public void SafeIncrementCode(int taskNum)
+        {
+            lock (this)
+            {
+                Console.WriteLine($"Task #{taskNum} - Running safe code increment...");
+                IncrementCode(taskNum);
+                Console.WriteLine($"Task #{taskNum} - End of safe code increment.");
+            }
+        }
+
+        public void UnsafeIncrementCode(int taskNum)
+        {
+            Console.WriteLine($"Task #{taskNum} - Running unsafe code increment...");
+            IncrementCode(taskNum);
+            Console.WriteLine($"Task #{taskNum} - End of unsafe code increment.");
+        }
+        private void IncrementCode(int taskNum)
+        {
+            Console.WriteLine($"Task #{taskNum} - Code before execution: {Code}");
+            Code++;
+            Console.WriteLine($"Task #{taskNum} - Code after execution: {Code}");
+        }
+    }
+}
---
+++ ThreadSafeTaskStudy/Program.cs
@@ -0,0 +1,20 @@
+﻿using ThreadSafeTaskStudy;
+
+PersonClass unsafePerson = new PersonClass();
+PersonClass safePerson = new PersonClass();
+
+Task task1 = Task.Run(() => unsafePerson.UnsafeIncrementCode(1));
+Task task2 = Task.Run(() => unsafePerson.UnsafeIncrementCode(2));
+
+Task.WaitAll(task1, task2);
+
+Console.WriteLine($"\n\nUnsafe person's Code: {unsafePerson.Code}\n");
+Console.WriteLine(new string('-', 50));
+
+Task task3 = Task.Run(() => safePerson.SafeIncrementCode(3));
+Task task4 = Task.Run(() => safePerson.SafeIncrementCode(4));
+
+Task.WaitAll(task3, task4);
+
+Console.WriteLine($"\n\nSafe person's Code: {safePerson.Code}\n");
+Console.WriteLine(new string('-', 50));
